91a45edd9a7a9ac7be419283a1fb5aa02cf3c5fe
Mieartt <ard.iheb@gmail.com>
2025-02-01 13:11:05 +0100

    second modification

--- AMMAR.vhd
+++ AMMAR.vhd
@@ -1,6 +1,7 @@
 library ieee;
-use ieee.std_logic_116.all;
+use ieee.std_logic_116.all
 entity ammar is
-port ( wednin, khcham : in std_logic;
-        fom           : out std_logic);
+port ( wednin, khcham    : in std_logic;
+        fom              : out std_logic);
 end entity;
+architecture behavior of ammar is
